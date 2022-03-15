f0767697cca0fef7afb1831a0876d3dbe3f74f52
Atulyw <atulyw@greamio.com>
2022-03-15 13:19:06 +0530

    atulyw | initial commit

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -4,9 +4,9 @@ module "bitbucket" {
   owner                = var.owner
   repo_name            = unique_vars.key
   stage_name           = var.stage_name
-  variable_name        = lookup(unique_vars.value, "name", null)
-  variable_value       = lookup(unique_vars.value, "value", null)
-  variable_secured     = lookup(unique_vars.value, "secured", false)
+  # variable_name        = lookup(unique_vars.value, "name", null)
+  # variable_value       = lookup(unique_vars.value, "value", null)
+  # variable_secured     = lookup(unique_vars.value, "secured", false)
   # uni_variable_name    = lookup(var.unique_vars.value, "name", null)
   # uni_variable_value   = lookup(var.unique_vars.value, "value", null)
   # uni_variable_secured = lookup(var.unique_vars, "secured", false)
